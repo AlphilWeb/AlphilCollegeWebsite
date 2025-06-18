5e79294fd8c60cd0aef01e364bc91fc5bca8300c
p.karari <kararikelvin2002@gmail.com>
2025-06-18 18:31:53 +0300

    Tailwind config file

---
+++ alphil/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', 
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'dark-green': '#1E4D2B', 
+      },
+    },
+  },
+  plugins: [],
+}
