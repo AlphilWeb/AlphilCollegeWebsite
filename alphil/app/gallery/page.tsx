5e17e8d2798493cee069784b810c09125b124afa
p.karari <kararikelvin2002@gmail.com>
2025-06-21 18:45:44 +0300

    gallery page

---
+++ alphil/app/gallery/page.tsx
@@ -0,0 +1,19 @@
+import Footer from '@/components/Footer'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <li className='text-[1.2rem] m-[2.5rem] '>Gallery</li>
+  
+       <h1 className='text-4xl m-[2.5rem] '>Education That Sets You Apart. How alphil played a part in shaping these students lives</h1>
+       <hr />
+
+      <div>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default page
