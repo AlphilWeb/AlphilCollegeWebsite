836a23854ed51099bb6fba6333960f133125dd29
p.karari <kararikelvin2002@gmail.com>
2025-06-30 14:18:37 +0300

    update 3

--- alphil/app/layout.tsx
+++ alphil/app/layout.tsx
@@ -1,20 +1,6 @@
-import type { Metadata } from "next";
 import "./globals.css"; 
 import Navbar from "@/components/Navbar";
 import { AuthProvider } from "@/app/context/AuthContext";
-
-export const metadata: Metadata = {
-  title: "Alphil | Home",
-  description: "Alphil College",
-  icons: {
-    icon: [
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-    ],
-    apple: '/apple-touch-icon.png',
-  },
-  manifest: '/site.webmanifest'
-};
 
 export default function RootLayout({
   children,
