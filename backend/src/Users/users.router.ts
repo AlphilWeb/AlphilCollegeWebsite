f29fa7613c05e144b01ffa1b7bfaa461ded305de
p-karari <kararikelvin2002@gmail.com>
2025-09-11 11:14:27 +0300

    Updates

--- backend/src/Users/users.router.ts
+++ backend/src/Users/users.router.ts
@@ -4,13 +4,14 @@ import { UsersController } from './users.controller';
 import { authMiddleware, adminMiddleware } from '../middleware/auth.middleware';
 
 const usersRouter = new Hono();
+usersRouter.post('/', UsersController.createUser); // POST /users
+
 
 // Apply middleware to all routes
 usersRouter.use('*', authMiddleware, adminMiddleware);
 
 // Routes
 usersRouter.get('/', UsersController.getAllUsers); // GET /users
-usersRouter.post('/', UsersController.createUser); // POST /users
 usersRouter.delete('/:id', UsersController.deleteUser); // DELETE /users/:id
 usersRouter.put('/:id', UsersController.updateUser); // PUT /users/:id
 
