8296db034e982820f4028e52c054c0cb009f0587
p-karari <kararikelvin2002@gmail.com>
2026-01-29 11:53:18 +0300

    update; form generation

--- backend/src/Users/users.router.ts
+++ backend/src/Users/users.router.ts
@@ -1,18 +1,18 @@
 // src/Users/users.router.ts
 import { Hono } from 'hono';
+import { adminMiddleware, authMiddleware } from '../middleware/auth.middleware';
 import { UsersController } from './users.controller';
-import { authMiddleware, adminMiddleware } from '../middleware/auth.middleware';
 
 const usersRouter = new Hono();
-usersRouter.post('/', UsersController.createUser); // POST /users
+usersRouter.post('/', UsersController.createUser); 
 
 
 // Apply middleware to all routes
 usersRouter.use('*', authMiddleware, adminMiddleware);
 
 // Routes
-usersRouter.get('/', UsersController.getAllUsers); // GET /users
-usersRouter.delete('/:id', UsersController.deleteUser); // DELETE /users/:id
-usersRouter.put('/:id', UsersController.updateUser); // PUT /users/:id
+usersRouter.get('/', UsersController.getAllUsers); 
+usersRouter.delete('/:id', UsersController.deleteUser);
+usersRouter.put('/:id', UsersController.updateUser); 
 
 export default usersRouter;
